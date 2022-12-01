0e8bd4571d16cceeecad0e3c7b47ae3c36ca58bd
Wojciech Zarychta <wojzarychta@gmail.com>
2022-12-01 19:37:15 +0100

    sps30 update and documentation

--- sps30.py
+++ sps30.py
@@ -1,6 +1,7 @@
 from i2c import *
 import struct
 from enum import Enum
+from time import sleep
 
 
 class SPS30:
@@ -19,13 +20,14 @@ class SPS30:
     _READ_SERIAL_NUMBER = [0xD0, 0x33]
     _READ_VERSION = [0xD1, 0x00]
 
+    MEASUREMENT_ERROR = -1
     _NO_ERROR = 1
-    _ARTICLE_CODE_ERROR = -1
+    _ARTICLE_CODE_ERROR = -7
     _SERIAL_NUMBER_ERROR = -2
     _AUTO_CLN_INTERVAL_ERROR = -3
     _DATA_READY_FLAG_ERROR = -4
     _MEASURED_VALUES_ERROR = -5
-    _VERSION_ERROR = -5
+    _VERSION_ERROR = -6
 
     class MeasType(Enum):
         IEEE754_TYPE = 0
@@ -33,16 +35,16 @@ class SPS30:
 
     chosen_meas_type = -1
 
-    dict_output = {"pm1p0": None,   # [ug/m3]
-                   "pm2p5": None,   # [ug/m3]
-                   "pm4p0": None,   # [ug/m3]
-                   "pm10p0": None,  # [ug/m3]
-                   "nc0p5": None,   # [#/cm3]
-                   "nc1p0": None,   # [#/cm3]
-                   "nc2p5": None,   # [#/cm3]
-                   "nc4p0": None,   # [#/cm3]
-                   "nc10p0": None,  # [#/cm3]
-                   "typical": None  # [um]- for float/[nm] - for uint
+    dict_output = {"mass concentration PM1.0 [ug/m3]": None,
+                   "mass concentration PM2.5 [ug/m3]": None,
+                   "mass concentration PM4 [ug/m3]": None,
+                   "mass concentration PM10 [ug/m3]": None,
+                   "number concentration PM0.5 [#/cm3]": None,
+                   "number concentration PM1.0 [#/cm3]": None,
+                   "number concentration PM2.5 [#/cm3]": None,
+                   "number concentration PM4 [#/cm3]": None,
+                   "number concentration PM10 [#/cm3]": None,
+                   "typical particle size": None  # [um]- for float/[nm] - for uint
                    }
 
     def __init__(self, i2c: I2C):
@@ -71,6 +73,10 @@ class SPS30:
         return True
 
     def read_device_serial(self):
+        """
+        function reading device's serial number
+        :return: device's serial number in hex as string
+        """
         device_serial = []
 
         self._i2c.write(self._ADDR, self._READ_SERIAL_NUMBER)
@@ -85,6 +91,10 @@ class SPS30:
             return self._SERIAL_NUMBER_ERROR
 
     def read_firmware_ver(self):
+        """
+        function reading device's firmware version
+        :return: number of version
+        """
         self._i2c.write(self._ADDR, self._READ_VERSION)
         buf = self._i2c.read(self._ADDR, 3)
 
@@ -93,7 +103,15 @@ class SPS30:
         else:
             return self._VERSION_ERROR
 
-    def start_measurement(self, out_format: MeasType):
+    def _start_measurement(self, out_format=MeasType.IEEE754_TYPE):
+        """
+        function which starts measurement cycle
+
+        :param out_format: format of measurement (int or float); default float
+        :return: none
+        """
+        self._wake_up()  # firstly wake up device from idle mode
+
         self.chosen_meas_type = out_format
 
         byte_arr = self._START_MEAS
@@ -102,17 +120,25 @@ class SPS30:
         elif out_format == self.MeasType.UINT_TYPE:
             byte_arr.append(0x05)
 
-        byte_arr.append(0x00)
+        byte_arr.append(0x00)  # dummy byte
 
         crc = self._calculateCRC(byte_arr[2:4])
         byte_arr.append(crc)
 
         self._i2c.write(self._ADDR, byte_arr)
 
-    def stop_measurement(self):
+    def _stop_measurement(self):
+        """
+        function which stops measurements and puts device in idle mode
+        :return: none
+        """
         self._i2c.write(self._ADDR, self._STOP_MEAS)
 
-    def read_measured_values(self):
+    def _read_measured_values(self):
+        """
+        function to read single measurement from device
+        :return: dictionary with measurements
+        """
         if self.chosen_meas_type == -1:
             raise Exception("Trying to read values before start of measurement")
 
@@ -126,50 +152,124 @@ class SPS30:
         byte_array = self._i2c.read(self._ADDR, bytes_to_read)
 
         if self._checkCRC(byte_array):
-            self._calculate_sensor_values(byte_array)
-            return self._NO_ERROR
+            measured_values = self._calculate_sensor_values(byte_array)
+            return measured_values
         else:
             return self._MEASURED_VALUES_ERROR
 
     def _calculate_sensor_values(self, byte_arr):
+        """
+        function which calculates actual values of measurement based on bytes received from device
+        :param byte_arr: array of bytes which come from device after measurement
+        :return: decoded measurement values as dictionary
+        """
+        values = dict(self.dict_output)
+
         if self.chosen_meas_type == self.MeasType.IEEE754_TYPE:
             i = 4
-            for d in self.dict_output:
+            for d in values:
                 # value is in IEEE 754 (sign, exponent and mantissa) format which needs to be parsed before writing
                 value = byte_arr[i] + byte_arr[i - 1] * pow(2, 8) + \
                         byte_arr[i - 3] * pow(2, 16) + byte_arr[i - 4] * pow(2, 24)
-                self.dict_output[d] = self._parse_IEEE754_to_float(value)
+                values[d] = self._parse_IEEE754_to_float(value)
                 i += 6
         elif self.chosen_meas_type == self.MeasType.UINT_TYPE:
             i = 1
-            for d in self.dict_output:
+            for d in values:
                 value = byte_arr[i] + byte_arr[i - 1] * pow(2, 8)
-                self.dict_output[d] = value
+                values[d] = value
                 i += 3
+
+        return values
 
     @staticmethod
     def _parse_IEEE754_to_float(value):
+        """
+        function which parses number from IEEE754 to float
+        :param value: number in IEEE754 format
+        :return: value as float
+        """
         string_value = str(hex(value)).replace("0x", "")
         byte_value = bytes.fromhex(string_value)
         return struct.unpack('>f', byte_value)[0]
 
-    def access_measured_values(self):
-        # returns dictionary
-        return self.dict_output
+    # def access_measured_values(self):
+    #     # returns dictionary
+    #     return self.dict_output
 
-    def read_data_ready_flag(self):
-        # check if data is ready to read in synchronous mode
-        # if so, return 1, else 0 or DATA_READY_FLAG_ERROR in case of wrong CRC
+    def measure(self, out_format=MeasType.IEEE754_TYPE):
+        """
+        function to measure particle matters implemented according to sps30 datasheet:
+        - after waking up device wait 30 sec
+        - then, for 30 sec read values once a sec
+        - take average from above 30 measurements
+        - put device to sleep
+        :param out_format:
+        :return: dictionary with correct measurements
+        """
+        self._start_measurement(out_format)
+        sleep(30)
+        i = 0
+        measurements = dict(self.dict_output)
+        while i < 30:  # get 30 measurements to calculate avg
+            while not self._read_data_ready_flag():  # wait for next values (approx. 1 sec)
+                pass
+            i += 1
+            readout = self._read_measured_values()
+            if readout == self._MEASURED_VALUES_ERROR:  # in case of error:
+                # reset device?
+                for i in measurements:
+                    measurements[i] = -1
+                    return measurements
+            else:
+                for i in measurements:
+                    measurements[i] += readout[i]
+        # calculate average
+        if self.chosen_meas_type == self.MeasType.IEEE754_TYPE:
+            for i in measurements:
+                measurements[i] /= 30
+        elif self.chosen_meas_type == self.MeasType.UINT_TYPE:
+            for i in measurements:
+                measurements[i] //= 30
+
+        self._stop_measurement()
+        return measurements
+
+    def print_measured_values(self):
+        """
+        print dictionary with measured values
+        :return: none
+        """
+        meas_dict = self.measure()
+        for i in meas_dict:
+            if i == "typical particle size":
+                if self.chosen_meas_type == self.MeasType.IEEE754_TYPE:
+                    print(i + " [um]" + ": " + str(meas_dict[i]))
+                elif self.chosen_meas_type == self.MeasType.UINT_TYPE:
+                    print(i + " [nm]" + ": " + str(meas_dict[i]))
+            else:
+                print(i + ": " + str(meas_dict[i]))
+
+    def _read_data_ready_flag(self):
+        """
+        check if data is ready to read in synchronous mode
+        if so, return 1, else 0 or DATA_READY_FLAG_ERROR in case of wrong CRC
+        :return: True if flag is ready, False otherwise
+        """
         self._i2c.write(self._ADDR, self._READ_DATA_RDY_FLAG)
         byte_arr = self._i2c.read(self._ADDR, 3)
         if self._checkCRC(byte_arr):
             # flag is stored in first byte of the returned array
-            return byte_arr[1]
+            return byte_arr[1] == 0x01
         else:
-            return self._DATA_READY_FLAG_ERROR
+            return False
 
-    def wake_up(self):
-        # in order to wake send wake-up cmd 2 times
+    def _wake_up(self):
+        """
+        wakes up device from idle mode
+        in order to wake send wake-up command 2 times
+        :return: none
+        """
         for i in range(2):
             self._i2c.write(self._ADDR, self._WAKE_UP)
 
